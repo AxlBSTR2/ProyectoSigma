c850ad6b8e006f7ef1129801cd86520ebf23fa75
unknown <ax.lange@duocuc.cl>
2023-07-05 20:37:33 -0400

    3.0.2

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Axel Python\Desktop\ProyectoSigma-axl\ProyectoSigma-axl\myvenv
+set VIRTUAL_ENV=C:\Users\Axel Python\Desktop\ProyectoSigma-main\ProyectoSigma-main\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
